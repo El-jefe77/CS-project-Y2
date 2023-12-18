62ab914d2e2e9cc8219aa71280613cbc6681c281
El-jefe77 <alexander.arguetajf@gmail.com>
2023-12-18 09:57:27 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from sys import exit
+
+#initialize pygame
+pygame.init()
+#creation of empty tab and size of the tab
+screen = pygame.display.set_mode((1200,800))
+pygame.display.set_caption("Card game")
+#set frame rate for an even speed across devices 
+clock = pygame.time.Clock()              
+
+#creation of surfaces 
+    #this variable can change the size and fill (content) of a surface
+pygame.image.load("link of image")
+
+#"while true" will create an infinite loop, meaning it will execute it indefinitely until the program is interrupted or terminated
+    #all operations inside the while loop are constantly updated 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+ #this surface determines position of the shape (surface)
+    screen.blit(background_surface,(0,0))
+
+    pygame.display.update()
+    clock.tick(60)
